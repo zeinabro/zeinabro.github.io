663c24a32f14eea6018451fb0c8f65e0d8291c2c
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-09-23 19:04:37 +0100

    projects title

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,6 +9,16 @@ async function show_projects() {
     console.log('show projects')
     const projects = await get_data()
     console.log(projects)
+    const projectList = document.createElement('ul')
+    projectList.className = 'project-list'
+    projects.forEach(project => {
+        const projectItem = document.createElement('div')
+        projectItem.className = 'project'
+        projectItem.textContent = project.name
+        projects_section.appendChild(projectItem)
+        console.log(projectList)
+    });
+    projects_section.append(projectList)
 }
 
 const projects_section = document.querySelector('.projects')
