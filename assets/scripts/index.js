b79b455d1831efa5a1a0f29083e4ce9c4b731152
Zeinab Roumieh <zeinabroumieh@outlook.com>
2023-09-29 10:38:05 +0100

    added role and description to frontend

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,10 +10,24 @@ async function show_projects() {
     const projects = await get_data()
     console.log(projects)
     projects.forEach(project => {
-        const projectItem = document.createElement('div')
-        projectItem.className = 'project'
-        projectItem.textContent = project.name
-        projects_section.appendChild(projectItem)
+        const project_item = document.createElement('div')
+        project_item.className = 'project'
+
+        const project_name = document.createElement('p')
+        project_name.className = 'title'
+        project_name.textContent = project.name
+
+        const project_role = document.createElement('span')
+        project_role.className = 'role'
+        project_role.textContent = project.role
+
+        const project_info = document.createElement('p')
+        project_info.className = 'info'
+        project_info.textContent = project.description
+        
+        project_item.append(project_name,project_role,project_info)
+        projects_section.appendChild(project_item)
+
     });
 }
 
